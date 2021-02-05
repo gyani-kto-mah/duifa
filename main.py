7723270c841bd47d428f99e2b5dbea368d75d896
नरे भाई <gyani.kto.mah@gmail.com>
2021-02-05 13:27:37 +0545

    add all

---
+++ main.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+from pyotp import TOTP
+from os import chdir, listdir
+from stoyled import *
+
+banner = """
+ ______       _______ ___  
+ |  _  \     (_)  ___/ _ \ 
+ | | | |_   _ _| |_ / /_\ \\
+ | | | | | | | |  _||  _  |
+ | |/ /| |_| | | |  | | | |
+ |___/  \__,_|_\_|  \_| |_/
+
+"""[1:-1]
+
+print(banner)
+print(info(f'Initialized [at] -> {fetchFormatedTime()}'))
+
+chdir('.secrets')
+
+secret_files = []
+
+for _ in listdir():
+    if  _.endswith('_secret.txt'):
+        secret_files.append(_)
+
+for _ in range(len(secret_files)):
+    print(info(f'{_} -> {secret_files[_][:-11]}'))
+
+choice = int(coolInput('App no'))
+
+if choice > _:
+    print(bad("Error -> Given App no doesn't exist."))
+    coolExit(1)
+
+secret_filename = secret_files[choice]
+print(info(f'App -> {secret_filename[:-11]}'))
+
+otp = TOTP(open(secret_filename).read().strip().replace(' ', ''))
+
+print(good(f'OTP -> {otp.now()}'))
+
+chdir('..')
+
+coolExit()
